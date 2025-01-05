fdb60624ca98161bd10428228b4ee475891f7b30
nplayer89 <n.player89@gmail.com>
2025-01-04 21:12:55 -0700

    added tests for health check

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use rusty_blog::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run()?.await
 }
