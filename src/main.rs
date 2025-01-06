801764370abced56eec93a3247319ada846c8f4a
nplayer89 <n.player89@gmail.com>
2025-01-05 21:16:35 -0700

    updated test to spawn server on random port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use rusty_blog::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health")
+        .get(&format!("{}/health", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,8 +15,12 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 
-    fn spawn_app() {
-        let server = rusty_blog::run().expect("Failed to bind address");
+    fn spawn_app() -> String {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+        let port = listener.local_addr().unwrap().port();
+
+        let server = rusty_blog::run(listener).expect("Failed to bind address");
         let _ = tokio::spawn(server);
+        format!("http://127.0.0.1:{}", port)
     }
 }
