175163b01728a2b2ea35eacb7be99ab01f799a0f
nplayer89 <n.player89@gmail.com>
2025-03-06 21:24:59 -0700

    added logging to main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ use rusty_blog::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port");
+    println!("running on port 8000");
     run(listener)?.await
 }
